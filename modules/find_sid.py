b021933872a740fe89306949b7d234e5deef94e6
Carter Dewey <cadewey@github.com>
2020-02-17 20:54:26 -0500

    Initial commit

---
+++ modules/find_sid.py
@@ -0,0 +1,14 @@
+# Looks for files in the specified path that have either the SUID
+# or SGID bit set. Potentially useful for identifying files that
+# could be attack vectors.
+
+def name():
+    return "find-sid"
+
+def args():
+    return ["path"]
+
+def run(args):
+    from subprocess import run, DEVNULL, PIPE
+    proc = run(["find", args.path, "-perm", "/u=s,g=s"], stdout=PIPE, stderr=DEVNULL)
+    print(proc.stdout.decode("utf-8"))
---
+++ pctf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import argparse
+import importlib
+from glob import glob
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description="PCTF helper tool")
+command_parser = parser.add_subparsers(title="Commands", dest="command")
+command_parser.required = True
+
+def load_modules():
+    for module in glob("./modules/*.py"):
+        module_name = "modules.{0}".format(Path(module).stem)
+        imported_module = importlib.import_module(module_name)
+
+        cmd = command_parser.add_parser(imported_module.name())
+        cmd.set_defaults(func=imported_module.run)
+        for arg in imported_module.args():
+            cmd.add_argument(arg)
+
+def main():
+    load_modules()
+
+    args = parser.parse_args()
+    args.func(args)
+
+if __name__ == "__main__":
+    main()
