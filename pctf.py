880955493fbf114db23dd899cd96fff63849941b
Carter Dewey <cadewey@github.com>
2020-02-18 22:04:59 -0500

    Add some error handling to the import loop

--- pctf.py
+++ pctf.py
@@ -11,13 +11,17 @@ command_parser.required = True
 
 def load_modules():
     for module in glob("./modules/*.py"):
-        module_name = "modules.{0}".format(Path(module).stem)
-        imported_module = importlib.import_module(module_name)
+        try:
+            module_name = "modules.{0}".format(Path(module).stem)
+            imported_module = importlib.import_module(module_name)
 
-        cmd = command_parser.add_parser(imported_module.name(), help=imported_module.help())
-        cmd.set_defaults(func=imported_module.run)
-        for arg in imported_module.args():
-            cmd.add_argument(arg)
+            cmd = command_parser.add_parser(imported_module.name(), help=imported_module.help())
+            cmd.set_defaults(func=imported_module.run)
+            for arg in imported_module.args():
+                cmd.add_argument(arg)
+        except e:
+            print("Unable to import module {0}".format(module))
+            print("Exception: {0}".format(e))
 
 def main():
     load_modules()
